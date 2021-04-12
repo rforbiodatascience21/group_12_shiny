96273518c681e99cd58dadd0e491a321d623972c
nrt94 <nrthornval@hotmail.com>
2021-04-12 09:57:33 +0000

    Awesome app

--- app_fra_youtube_tut.R
+++ app_fra_youtube_tut.R
@@ -14,6 +14,42 @@
 library(shiny)
 library(shinythemes)
 
+#functions:
+complement <- function(dna){
+  lookup <- c("A" = "T", "T" = "A", "G" = "C", "C" = "G") # named vector, think key/value
+  dna_split <- strsplit(dna, "")[[1]]
+  dna_complement <- paste0(lookup[dna_split], collapse = "")
+  return(dna_complement)
+} 
+
+mk_codons <- function(dna, s = 1){
+  l = nchar(dna)
+  codons <- substring(dna,
+                      first = seq(from = s, to = l-3+1, by = 3),
+                      last = seq(from = 3+s-1, to = l, by = 3))
+  return(codons)
+}
+
+dna_codons_to_aa <- function(codons){
+  std_code_table <- c("TTT" = "F", "TCT" = "S", "TAT" = "Y", "TGT" = "C",
+                      "TTC" = "F", "TCC" = "S", "TAC" = "Y", "TGC" = "C",
+                      "TTA" = "L", "TCA" = "S", "TAA" = "*", "TGA" = "*",
+                      "TTG" = "L", "TCG" = "S", "TAG" = "*", "TGG" = "W",
+                      "CTT" = "L", "CCT" = "P", "CAT" = "H", "CGT" = "R",
+                      "CTC" = "L", "CCC" = "P", "CAC" = "H", "CGC" = "R",
+                      "CTA" = "L", "CCA" = "P", "CAA" = "Q", "CGA" = "R",
+                      "CTG" = "L", "CCG" = "P", "CAG" = "Q", "CGG" = "R",
+                      "ATT" = "I", "ACT" = "T", "AAT" = "N", "AGT" = "S",
+                      "ATC" = "I", "ACC" = "T", "AAC" = "N", "AGC" = "S",
+                      "ATA" = "I", "ACA" = "T", "AAA" = "K", "AGA" = "R",
+                      "ATG" = "M", "ACG" = "T", "AAG" = "K", "AGG" = "R",
+                      "GTT" = "V", "GCT" = "A", "GAT" = "D", "GGT" = "G",
+                      "GTC" = "V", "GCC" = "A", "GAC" = "D", "GGC" = "G",
+                      "GTA" = "V", "GCA" = "A", "GAA" = "E", "GGA" = "G",
+                      "GTG" = "V", "GCG" = "A", "GAG" = "E", "GGG" = "G")
+  aa <- paste0(std_code_table[codons], collapse = "")
+  return(aa)
+}
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("cerulean"),
@@ -24,18 +60,20 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                            sidebarPanel(
                              tags$h3("Input:"),
                              textInput("txt1", "DNA sequence:", ""),
-                             #textInput("txt2", "Surname:", ""),
-                             
+              
+                             #actionbar
+                             actionButton("com", "Complementary DNA"),
+                             actionButton("codon", "Make codons")
                            ), # sidebarPanel
                            mainPanel(
-                             h1("Header 1"),
+                             h1("Output"),
                              
-                             h4("Output 1"),
+                             h4("Result"),
                              verbatimTextOutput("txtout"),
                              
                            ) # mainPanel
                            
-                  ), # Navbar 1, tabPanel
+                  ) # Navbar 1, tabPanel
                   #tabPanel("Navbar 2", "This panel is intentionally left blank"),
                   #tabPanel("Navbar 3", "This panel is intentionally left blank")
                   
@@ -45,9 +83,18 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
 
 # Define server function  
 server <- function(input, output) {
+  v <- reactiveValues(data = NULL)
+  
+  observeEvent(input$com, {
+    v$data <- complement(input$txt1)
+  })
+  
+  observeEvent(input$codon, {
+    v$data <- mk_codons(input$txt1)
+  })
   
   output$txtout <- renderText({
-    paste( input$txt1, input$txt2, sep = " " )
+    paste( v$data,sep = " " )
   })
 } # server
 
